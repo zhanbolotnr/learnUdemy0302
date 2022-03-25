4acb43cc990010631bc63ae6cd45e2fb7adfe2ad
Zhanbolot <zhanbolotnr@gmail.com>
2022-03-25 23:40:18 +0600

    вложенныйциклfor

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LearnUdemy0302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Вложенный цикл for
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}). Indexes ({i};{j})");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+                        int atI = numbers[i];
+                        int atJ = numbers[j];
+                        int atK = numbers[k];
+
+                        if (atI + atJ + atK == 0)
+                        {
+                            Console.WriteLine($"Pair ({atI};{atJ}:{atK}). Indexes ({i};{j};{k})");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
